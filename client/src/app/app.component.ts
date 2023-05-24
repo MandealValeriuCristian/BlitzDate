f294195ff32ae8e291cf595ecc50b4d50eacf321
Valeriu-Cristian Mandeal <mandealvaleriucristian@gmail.com>
2023-05-24 14:31:53 +0300

    59. Parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -10,19 +9,11 @@ import { AccountService } from './_services/account.service';
 })
 export class AppComponent implements OnInit {
   title = 'BlitzDate';
-  users: any;
-  constructor(private http: HttpClient, private accountService: AccountService) {}  
+  constructor(private accountService: AccountService) {}  
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+
   setCurrentUser() {
     const userString = localStorage.getItem('user');
     if(!userString) return;
