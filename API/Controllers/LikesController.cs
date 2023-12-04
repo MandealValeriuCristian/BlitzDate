1fca72fd7ed719d6aafc65dad3e6fa953994367e
Cristian Mandeal <cristianm@directferries.com>
2023-12-04 17:07:44 +0200

    180. Adding pagination for the likes

--- API/Controllers/LikesController.cs
+++ API/Controllers/LikesController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,7 +20,7 @@ public class LikesController : BaseApiController
     [HttpPost("{username}")]
     public async Task<ActionResult> AddLike(string username)
     {
-        var sourceUserId = int.Parse(User.GetUserId());
+        var sourceUserId = User.GetUserId();
         var likedUser = await _userRepository.GetUserbyUsernameAsync(username);
         var sourceUser = await _likesReporsitory.GetUserWithLikes(sourceUserId);
 
@@ -44,9 +45,12 @@ public class LikesController : BaseApiController
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+    public async Task<ActionResult<PagedList<LikeDto>>> GetUserLikes([FromQuery]LikesParams likesParams)
     {
-        var users = await _likesReporsitory.GetUserLikes(predicate, int.Parse(User.GetUserId()));
+        likesParams.UserId = User.GetUserId();
+        var users = await _likesReporsitory.GetUserLikes(likesParams);
+        Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize,
+             users.TotalCount, users.TotalPages));
         return Ok(users);
     }
 
