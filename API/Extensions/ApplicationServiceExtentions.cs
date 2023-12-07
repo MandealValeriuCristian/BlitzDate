c41fd31eb76818aa690dd7cd821b3fd2b27fd4dd
Cristian Mandeal <cristianm@directferries.com>
2023-12-07 11:35:21 +0200

    185. Setting up the message repository

--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -27,6 +27,7 @@ namespace API.Extensions
                 services.AddScoped<IPhotoService, PhotoService>();
                 services.AddScoped<LogUserActivity>();
                 services.AddScoped<ILikesReporsitory, LikesRepository>();
+                services.AddScoped<IMessageRepository, MessageRepository>();
                 return services;
             }
     }
