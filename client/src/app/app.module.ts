e9549dc38cc27e398b791c582eb933b914e885a3
Valeriu-Cristian Mandeal <mandealvaleriucristian@gmail.com>
2024-01-20 20:18:47 +0200

    214. Adding an admin component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { DatePickerComponent } from './_forms/date-picker/date-picker.component'
     MemberEditComponent,
     PhotoEditorComponent,
     TextInputComponent,
-    DatePickerComponent
+    DatePickerComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserAnimationsModule,
