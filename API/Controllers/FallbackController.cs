b8e527f2f7efcdc26fb524f060d94a66dc52cacc
Valeriu-Cristian Mandeal <mandealvaleriucristian@gmail.com>
2024-02-04 17:19:43 +0200

    250. Adding a fallback controller

---
+++ API/Controllers/FallbackController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("[controller]")]
+public class FallbackController : Controller
+{
+    
+    public ActionResult Index()
+    {
+        return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+    }
+}
